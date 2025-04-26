2769b08fedfb9a46d7e30477db76315cd17289d4
unknown564 <da24b028@smail.iitm.ac.in>
2025-04-26 14:41:06 +0530

    help_pls

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="OptiWave",
     version="0.1.0",
-    packages=find_packages(where="src"),
+    packages=["src"],
     package_dir={"OptiWave": "src"},
     install_requires=[
         'numpy>=1.18.0',  # Example of a dependency
